940bd31651b051721fb30ced734b3f4afcc0b2d2
kamiyalog <genkiterui0896@gmail.com>
2026-06-22 00:52:27 +0900

    Add files via upload

--- search.js
+++ search.js
@@ -3,6 +3,10 @@ function search() {
 
   if (q === "月白大学") {
     window.location.href = "deleted-blog-01.html";
+} else if (q === "記憶残留現象と共同幻想") {
+  window.location.href = "research-presentation.html";
+}
+
   } else {
     alert("該当する記事は見つかりませんでした");
   }
